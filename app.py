8d52f51fa7ad4b4c9bba81dcef905c70873ed5bb
cdeimer-via <cdeimer@viascience.com>
2023-06-27 11:01:30 -0400

    app home formatting

--- app.py
+++ app.py
@@ -16,47 +16,55 @@ def update_home_tab(client, event, logger):
   try:
     # views.publish is the method that your app uses to push a view to the Home tab
     client.views_publish(
-      # the user that opened your app's app home
-      user_id=event["user"],
-      # the view object that appears in the app home
-      view={
-        "type": "home",
-        "callback_id": "home_view",
+        # the user that opened your app's app home
+        user_id=event["user"],
+        # the view object that appears in the app home
+        view={
+            "type": "home",
+            "callback_id": "home_view",
 
-        # body of the view
-        "blocks": [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_* :rev-up-those-fryers:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me!"
+            # body of the view
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*Welcome to VIAGPT in Slack!* :rev-up-those-fryers:"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Navigate to the \"Messages\" tab to begin chatting with our custom VIAGPT instance. You can ask it questions about VIA components and services, with more information on the way."
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*Technical Information*\n • <https://github.com/cdeimer-via/viagpt-slack-app|Slack App Repo> \n • <https://github.com/viascience/viagpt-backend|VIAGPT Backend>"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*Security Disclaimer*\n Disclaimer: The VIAGPT Slack App ensures your data’s confidentiality within Slack, without sending or storing it elsewhere. We prioritize privacy and adhere to a strict no-data-sharing policy."
+                    }
                 }
-              }
             ]
-          }
-        ]
-      }
-    )
+        }
+    )       
 
   except Exception as e:
     logger.error(f"Error publishing home tab: {e}")
